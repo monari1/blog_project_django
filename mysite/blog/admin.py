866550235009420946a148e856c23f73c48a2d70
monari1 <francisnyasinga@gmail.com>
2024-01-11 21:52:38 +0300

    some changes to the admin site

--- mysite/blog/admin.py
+++ mysite/blog/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Post
+
 
 # Register your models here.
+admin.site.register(Post)
+
+
+# .............. Part 1 (Adding model to admin site)..................
+# first we need to add model to the admin site as shown in part 1
+
