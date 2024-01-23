72c709e9b50a111690f38f67ad930adf7d6e1873
monari1 <francisnyasinga@gmail.com>
2024-01-23 23:31:25 +0300

    views completed

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from .models import Post
+from django.http import Http404
 
-# Create your views here.
+def post_list(request):
+    post = Post.published.all()
+    return render(request, 'blog/post/list.html', {'post': post})
+def post_detail(request, id):
+    try: 
+        post = Post.published.get(id=id)
+    except Post.DoesNotExist:
+        raise Http404("No post found")
+    return render(request, 'blog/post/detail.html', {'post': post}) 
