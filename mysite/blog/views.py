203c274f811175a9681c78f05f11e4724e58ccea
monari1 <francisnyasinga@gmail.com>
2024-02-08 23:03:19 +0300

    some changes on posts

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -2,8 +2,9 @@ from django.shortcuts import render, get_object_or_404
 from .models import Post
 
 def post_list(request):
-    post = Post.published.all()
-    return render(request, 'blog/post/list.html', {'post': post})
+    posts = Post.published.all()
+    return render(request, 'blog/post/list.html', {'posts': posts})
+
 def post_detail(request, id):
     post = get_object_or_404(Post, id = id, status = Post.Status.PUBLISHED)
    
