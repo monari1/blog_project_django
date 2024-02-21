893ea2e38b8c72b6e0a5fb63d31174c27353f0db
monari1 <francisnyasinga@gmail.com>
2024-02-21 11:25:22 +0300

    changes commited

---
+++ springidol_3/src/main/java/com/example/springidol_3/Springidol3Application.java
@@ -0,0 +1,13 @@
+package com.example.springidol_3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springidol3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springidol3Application.class, args);
+	}
+
+}
